4a0ce6ee5f03efab5252a454856977e5ad9f1855
vicpatino <rosariopatinoe@gmail.com>
2024-08-29 18:22:49 -0400

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -12,6 +12,23 @@ sys.path.append("..")
 from data.import_dataset import load_data
 from data.features import preprocess
 
+numeric_feat = [
+    "pickup_weekday",
+    "pickup_hour",
+    'work_hours',
+    "pickup_minute",
+    "passenger_count",
+    'trip_distance',
+    'trip_time',
+    'trip_speed'
+]
+categorical_feat = [
+    "PULocationID",
+    "DOLocationID",
+    "RatecodeID",
+]
+features = numeric_feat + categorical_feat
+
 #importar modelo:
 loaded_rfc = joblib.load("random_forest.joblib")
 
